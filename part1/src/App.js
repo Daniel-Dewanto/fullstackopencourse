acb1fc6d4a8afb04124fc0a16bb02de102ee28b4
danieldewanto <daniel.dewanto1111@gmail.com>
2022-07-16 21:32:03 +1000

    finished part 1a code

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,11 +1,34 @@
-const App = () =>{
-  console.log('Hello from component')
+const Hello = (props)=>{
   return (
     <div>
       <p>
-        Hello World!
+        Hello {props.name}, you are {props.age} years old!
       </p>
     </div>
+  )
+}
+
+const Footer = ()=>{
+  return (
+    <div>
+      Greeting app created by <a href="https://github.com/daniel-dewanto" target="_blank" rel="noreferrer" >Daniel Dewanto</a>
+    </div>
+  )
+} 
+
+const App = () =>{
+  const nameDaniel = "Daniel"
+  const ageDaniel = 20
+  return (
+    <>
+      <h1>
+        Greetings!
+      </h1>
+      <Hello name={nameDaniel} age={ageDaniel}/>
+      <Hello name="Fidelio"/>
+      <Hello name="Eduardus"/>
+      <Footer/>
+    </>
   )
 }
 
